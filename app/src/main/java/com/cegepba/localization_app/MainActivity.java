df2f39ee01ed242fc085112740c1721668ecc983
Nicolas Bégin <beg83@hotmail.fr>
2020-03-11 16:06:26 -0400

    added initial map to project

--- app/src/main/java/com/cegepba/localization_app/MainActivity.java
+++ app/src/main/java/com/cegepba/localization_app/MainActivity.java
@@ -1,16 +1,40 @@
 package com.cegepba.localization_app;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.esri.arcgisruntime.mapping.view.MapView;
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
 import android.os.Bundle;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private MapView mMapView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //our first toast!!!
         Toast.makeText(MainActivity.this, "Firebase connection Sucess", Toast.LENGTH_LONG).show();
+
+        mMapView = findViewById(R.id.mapView);
+        ArcGISMap map = new ArcGISMap(Basemap.Type.TOPOGRAPHIC, 34.056295, -117.195800, 16);
+        mMapView.setMap(map);
+    }
+    @Override
+    protected void onPause(){
+        mMapView.pause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        mMapView.resume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mMapView.dispose();
     }
 }
---
+++ app/src/main/java/com/cegepba/localization_app/MapView.java
@@ -0,0 +1,4 @@
+package com.cegepba.localization_app;
+
+class MapView {
+}
