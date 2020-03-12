a648983a92083caa9bd9ba10b86d541f4f40f8c5
Mickael Bergeron <mickaelbergeron14@gmail.com>
2020-03-12 11:07:30 -0400

    Added legend activity

--- app/src/main/java/com/cegepba/localization_app/Legend.java
+++ app/src/main/java/com/cegepba/localization_app/Legend.java
@@ -4,12 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Info extends AppCompatActivity {
+public class Legend extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_info);
+        setContentView(R.layout.activity_legend);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
