9131b4ccc0a88bce467a4b60fed37096dc7ad1d9
Mickael Bergeron <mickaelbergeron14@gmail.com>
2020-03-13 16:09:03 -0400

    Modified code for popup

--- app/src/main/java/com/cegepba/localization_app/Pop.java
+++ app/src/main/java/com/cegepba/localization_app/Pop.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.widget.TextView;
 
 public class Pop extends AppCompatActivity {
+
+    private TextView roomName;
+    private TextView descriptionText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +23,11 @@ public class Pop extends AppCompatActivity {
         int height = displayMetrics.heightPixels;
 
         getWindow().setLayout((int)(width*.8),(int)(height*.6));
+
+        roomName = findViewById(R.id.roomId);
+        descriptionText = findViewById(R.id.descriptionText);
+
+        roomName.setText("B-534");
+        descriptionText.setText("Local Informatique");
     }
 }
