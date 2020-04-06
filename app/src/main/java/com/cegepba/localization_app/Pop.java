bcfe212f2159057f2c1dea1c50928edfdc70acca
Mickaël Bergeron <mickaelbergeron14@gmail.com>
2020-04-06 13:52:24 -0400

    Added class, added get for rooms [work-in-progress]

--- app/src/main/java/com/cegepba/localization_app/Pop.java
+++ app/src/main/java/com/cegepba/localization_app/Pop.java
@@ -1,12 +1,24 @@
 package com.cegepba.localization_app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 public class Pop extends AppCompatActivity {
+
+    private FirebaseFirestore firebaseFirestore;
 
     private TextView roomName;
     private TextView descriptionText;
@@ -15,6 +27,8 @@ public class Pop extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pop);
+
+        firebaseFirestore = FirebaseFirestore.getInstance();
 
         DisplayMetrics displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
@@ -27,7 +41,30 @@ public class Pop extends AppCompatActivity {
         roomName = findViewById(R.id.roomId);
         descriptionText = findViewById(R.id.descriptionText);
 
-        roomName.setText("B-534");
-        descriptionText.setText("Local Informatique");
+        setTextView();
+    }
+
+    private void setTextView() {
+        firebaseFirestore.collection("Rooms")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                DocumentReference docRef = firebaseFirestore.collection("Rooms").document(document.getId());
+                                docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                                    @Override
+                                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                        Rooms room = documentSnapshot.toObject(Rooms.class);
+
+                                        roomName.setText(room.getName());
+                                        descriptionText.setText(room.getDescription());
+                                    }
+                                });
+                            }
+                        }
+                    }
+                });
     }
 }
