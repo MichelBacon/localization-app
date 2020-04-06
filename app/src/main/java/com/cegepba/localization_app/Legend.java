cf15df88a0cb5c6a437d29e628c07b478cb9f4ca
Mickaël Bergeron <mickaelbergeron14@gmail.com>
2020-04-06 12:54:34 -0400

    Added get legends color and name and setText for textview

--- app/src/main/java/com/cegepba/localization_app/Legend.java
+++ app/src/main/java/com/cegepba/localization_app/Legend.java
@@ -1,34 +1,32 @@
 package com.cegepba.localization_app;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 public class Legend extends AppCompatActivity {
 
     private FirebaseFirestore firebaseFirestore;
     private TextView textViewColor;
     private TextView textViewName;
-    private List<Legends> legends;
+    private ArrayList<Legends> legends;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,17 +48,31 @@ public class Legend extends AppCompatActivity {
     }
 
     private void setTextView() {
-        /*firebaseFirestore.collection("Legends")
+        firebaseFirestore.collection("Legends")
                 .get()
                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
                     @Override
                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
                         if (task.isSuccessful()) {
                             for (QueryDocumentSnapshot document : task.getResult()) {
-                                 legends.add(document.toObject(Legends.class));
+                                DocumentReference docRef = firebaseFirestore.collection("Legends").document(document.getId());
+                                docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                                    @Override
+                                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                                        Legends legend = documentSnapshot.toObject(Legends.class);
+
+                                        if(legend != null)
+                                        {
+                                            legends = new ArrayList<>();
+                                            legends.add(legend);
+                                            textViewColor.setText(legends.get(0).getColor());
+                                            textViewName.setText(legends.get(0).getName());
+                                        }
+                                    }
+                                });
                             }
                         }
                     }
-                });*/
+                });
     }
 }
