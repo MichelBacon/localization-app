0deeccaa1b4219a2dacca228114113c6f2161735
Mickaël Bergeron <mickaelbergeron14@gmail.com>
2020-04-07 15:23:20 -0400

    Minor changes

--- app/src/main/java/com/cegepba/localization_app/MainActivity.java
+++ app/src/main/java/com/cegepba/localization_app/MainActivity.java
@@ -6,32 +6,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.DragEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.github.chrisbanes.photoview.OnViewDragListener;
 import com.github.chrisbanes.photoview.PhotoView;
 
+import java.lang.reflect.Array;
+
 public class MainActivity extends AppCompatActivity {
 
 
-    PhotoView photoView;
+    private PhotoView photoView;
+    private ImageView location_user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-      
+
+        location_user = findViewById(R.id.location_pointer);
         photoView = findViewById(R.id.photo_view);
         photoView.setImageResource(R.drawable.image);
+
+        photoView.setOnDragListener(new View.OnDragListener() {
+            @Override
+            public boolean onDrag(View v, DragEvent event) {
+                return false;
+            }
+        });
 
         photoView.setOnViewDragListener(new OnViewDragListener() {
             @Override
             public void onDrag(float dx, float dy) {
-
-                Log.e("DRAAAAAGGGG!!!!!", "position: = " + dy);
+                Log.e("DRAAAAAGGGG!!!!!", "onDrag: dy= " + photoView.getX());
+                location_user.setX(location_user.getX() + dx);
+                location_user.setY(location_user.getY() + dy);
             }
         });
     }
