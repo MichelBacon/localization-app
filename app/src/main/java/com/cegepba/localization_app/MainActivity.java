bbed5947057ec0ba5cf25705d06023527513f75d
Mickaël Bergeron <mickaelbergeron14@gmail.com>
2020-04-07 11:46:11 -0400

    Initialized the photoview to be size at the center of the screen

--- app/src/main/java/com/cegepba/localization_app/MainActivity.java
+++ app/src/main/java/com/cegepba/localization_app/MainActivity.java
@@ -5,21 +5,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import com.github.chrisbanes.photoview.OnViewDragListener;
 import com.github.chrisbanes.photoview.PhotoView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    PhotoView photoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
       
-        PhotoView photoView = findViewById(R.id.photo_view);
+        photoView = findViewById(R.id.photo_view);
         photoView.setImageResource(R.drawable.image);
+
+        photoView.setOnViewDragListener(new OnViewDragListener() {
+            @Override
+            public void onDrag(float dx, float dy) {
+
+                Log.e("DRAAAAAGGGG!!!!!", "position: = " + dy);
+            }
+        });
     }
 
     @Override
