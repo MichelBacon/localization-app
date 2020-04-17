cae1588217c33a68fb34d9e5127d32c6cdd7b199
Mickaël Bergeron <mickaelbergeron14@gmail.com>
2020-04-17 16:03:25 -0400

    Added Login for admin and added Ajout de locaux page

---
+++ app/src/main/java/com/cegepba/localization_app/Manager/AdminManager.java
@@ -0,0 +1,16 @@
+package com.cegepba.localization_app.Manager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cegepba.localization_app.R;
+
+public class AdminManager extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_manager);
+    }
+}
